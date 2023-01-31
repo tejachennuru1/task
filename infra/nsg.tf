4739de71d0dc51b698057ed21488a6c9abb4a992
murrali krishna <pudivikash@gmail.com>
2023-02-01 03:51:19 +0530

    36

---
+++ infra/nsg.tf
@@ -0,0 +1,25 @@
+resource "azurerm_network_security_group" "nsg" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.tony.location
+  resource_group_name = azurerm_resource_group.tony.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+resource "azurerm_subnet_network_security_group_association" "nsga" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
